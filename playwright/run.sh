4545167d14934f8ca70fb14466e7e9589811a5d5
Aref Shafaei <aref.shafaei@gmail.com>
2024-04-08 18:18:52 -0700

    change default page size

--- playwright/run.sh
+++ playwright/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export LOAD_TEST_RUN_COUNT=5
-export LOAD_TEST_PAGE_SIZE=50
+export LOAD_TEST_PAGE_SIZE=100
 export LOAD_TEST_CHAISE_URL="https://staging.atlas-d2k.org/chaise/recordset/\#2/Gene_Expression:Image/*::facets::N4IghgdgJiBcDaoDOB7ArgJwMYFM4gBUALNAWwCMIwBLAGwH0AhATwBcckQAaEDSAcw5xEIUtQj4AjAAYATABZuo8fRwAPLLTRJqANzyxWGNDgC+AXQumgA@sort(RID)"
 export LOAD_TEST_ERMREST_MAIN_URL_PREFIX="https://staging.atlas-d2k.org/ermrest/catalog/2/attributegroup/M:=Gene_Expression:Image/Thumbnail_Bytes::gt::1024"
 export LOAD_TEST_HATRAC_URL_PREFIX="https://staging.atlas-d2k.org/hatrac/resources/gene_expression"
