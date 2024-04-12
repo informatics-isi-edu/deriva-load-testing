a074bea37190fac96f7f3a4cbf3774be56bfa8d8
Aref Shafaei <aref.shafaei@gmail.com>
2024-04-12 11:05:10 -0700

    revert back to testing on staging

--- playwright/run-unsigned.sh
+++ playwright/run-unsigned.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 export LOAD_TEST_SIGNED=false
-export LOAD_TEST_RUN_COUNT=5
+export LOAD_TEST_RUN_COUNT=10
 export LOAD_TEST_PAGE_SIZE=100
-export LOAD_TEST_CHAISE_URL="https://www.atlas-d2k.org/chaise/recordset/#2/Gene_Expression:Image/Thumbnail_Bytes::gt::2048@sort(RID)"
-export LOAD_TEST_ERMREST_MAIN_URL_PREFIX="https://www.atlas-d2k.org/ermrest/catalog/2/attributegroup/M:=Gene_Expression:Image/Thumbnail_Bytes::gt::2048"
-export LOAD_TEST_HATRAC_URL_PREFIX="https://www.atlas-d2k.org/hatrac/resources/gene_expression"
+export LOAD_TEST_CHAISE_URL="https://staging.atlas-d2k.org/chaise/recordset/#2/Gene_Expression:Image/Thumbnail_Bytes::gt::1024@sort(RID)"
+export LOAD_TEST_ERMREST_MAIN_URL_PREFIX="https://staging.atlas-d2k.org/ermrest/catalog/2/attributegroup/M:=Gene_Expression:Image/Thumbnail_Bytes::gt::1024"
+export LOAD_TEST_HATRAC_URL_PREFIX="https://staging.atlas-d2k.org/hatrac/resources/gene_expression"
 
 npx playwright test image.spec.ts
