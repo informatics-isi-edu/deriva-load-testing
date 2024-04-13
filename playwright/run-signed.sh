4fb776deb3808076e1e295030a5a907b6127b39b
Aref Shafaei <aref.shafaei@gmail.com>
2024-04-12 18:13:37 -0700

    change run count to 10

--- playwright/run-signed.sh
+++ playwright/run-signed.sh
@@ -13,7 +13,7 @@ export LOAD_TEST_CHAISE_URL="https://staging.atlas-d2k.org/chaise/recordset/#2/G
 # 512K
 # export LOAD_TEST_CHAISE_URL="https://staging.atlas-d2k.org/chaise/recordset/#2/Gene_Expression:Image/*::facets::N4IghgdgJiBcDaoDOB7ArgJwMYFMDWOAnnCEjmNgBYC0ARigB4gA0p5Vc8IAKpWgLa0IYAJYAbagFYAjACYA0iAC6AX1VA@sort(RID)"
 
-export LOAD_TEST_RUN_COUNT=1
+export LOAD_TEST_RUN_COUNT=10
 export LOAD_TEST_PAGE_SIZE=100
 
 export LOAD_TEST_ERMREST_MAIN_URL_PREFIX="https://staging.atlas-d2k.org/ermrest/catalog/2/attributegroup/M:=Gene_Expression:Image/*::ciregexp::Thumbnail"
