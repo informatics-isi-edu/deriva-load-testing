458636f3cabac2709cddf467eed337ab3d5d1edf
Aref Shafaei <aref.shafaei@gmail.com>
2024-06-14 17:22:33 -0700

    commit the actual experiment for testing orchestrator

--- playwright/case-1-experiment.sh
+++ playwright/case-1-experiment.sh
@@ -28,16 +28,18 @@ export LOAD_TEST_RELOAD_COUNT=10
 
 echo "case-1-experiment.sh with signed: $1"
 
-for ((n=0;n<RUN_COUNT;n++)); do
-  for URL in "${URLs[@]}"; do
-    FILE_SIZE_LABEL=${URL%%:*}
-    CHAISE_URL=${URL#*:}
-    for PAGE_SIZE in ${PAGE_SIZES[*]}; do
-      export LOAD_TEST_FILE_SIZE_LABEL="$FILE_SIZE_LABEL"
-      export LOAD_TEST_CHAISE_URL="$CHAISE_URL"
-      export LOAD_TEST_PAGE_SIZE="$PAGE_SIZE"
-      echo "file size label: $LOAD_TEST_FILE_SIZE_LABEL"
-      time npx playwright test image.spec.ts --workers 1
-    done
-  done
-done
+node --version
+
+# for ((n=0;n<RUN_COUNT;n++)); do
+#   for URL in "${URLs[@]}"; do
+#     FILE_SIZE_LABEL=${URL%%:*}
+#     CHAISE_URL=${URL#*:}
+#     for PAGE_SIZE in ${PAGE_SIZES[*]}; do
+#       export LOAD_TEST_FILE_SIZE_LABEL="$FILE_SIZE_LABEL"
+#       export LOAD_TEST_CHAISE_URL="$CHAISE_URL"
+#       export LOAD_TEST_PAGE_SIZE="$PAGE_SIZE"
+#       echo "file size label: $LOAD_TEST_FILE_SIZE_LABEL"
+#       time npx playwright test image.spec.ts --workers 1
+#     done
+#   done
+# done
